8184bbf3699a5729ed197403107376c367338035
HwangJiYoung <start014stop@naver.com>
2026-08-11 22:47:10 +0900

    feat: 워크스페이스 참여 화면 구현

---
+++ src/features/workspace/pages/JoinWorkspacePage/JoinWorkspacePage.jsx
@@ -0,0 +1,12 @@
+import MainLayout from "../../../../components/MainLayout/MainLayout";
+import JoinWorkspaceForm from "../../components/JoinWorkspaceForm/JoinWorkspaceForm";
+
+function JoinWorkspacePage() {
+  return (
+    <MainLayout>
+      <JoinWorkspaceForm />
+    </MainLayout>
+  );
+}
+
+export default JoinWorkspacePage;
