3c3a3c174cc28a6b389b7cf985b1ddca934d08b2
HwangJiYoung <start014stop@naver.com>
2026-08-11 22:46:56 +0900

    feat: 워크스페이스 생성 화면 구현

---
+++ src/features/workspace/pages/CreateWorkspacePage/CreateWorkspacePage.jsx
@@ -0,0 +1,12 @@
+import MainLayout from "../../../../components/MainLayout/MainLayout";
+import CreateWorkspaceForm from "../../components/CreateWorkspaceForm/CreateWorkspaceForm";
+
+function CreateWorkspacePage() {
+  return (
+    <MainLayout>
+      <CreateWorkspaceForm />
+    </MainLayout>
+  );
+}
+
+export default CreateWorkspacePage;
