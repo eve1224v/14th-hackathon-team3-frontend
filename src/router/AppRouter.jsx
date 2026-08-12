872506fd39e8225794fc8838edf4de10cfe04728
HwangJiYoung <start014stop@naver.com>
2026-08-12 16:35:30 +0900

    feat: 라우팅 추가

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainPage from "../features/main/pages/MainPage/MainPage";
+
 import CreateWorkspacePage from "../features/workspace/pages/CreateWorkspacePage/CreateWorkspacePage";
 import JoinWorkspacePage from "../features/workspace/pages/JoinWorkspacePage/JoinWorkspacePage";
+
 import CreateProjectPage from "../features/project/pages/CreateProjectPage/CreateProjectPage";
 import ProjectSettingsPage from "../features/project/pages/ProjectSettingsPage/ProjectSettingsPage";
+
+import TimezoneSettingsPage from "../features/project/pages/TimezoneSettingsPage/TimezoneSettingsPage";
+import MemberSettingsPage from "../features/project/pages/MemberSettingsPage/MemberSettingsPage";
+import IntegrationSettingsPage from "../features/project/pages/IntegrationSettingsPage/IntegrationSettingsPage";
 
 import { ROUTES } from "./routes.constant";
 
@@ -19,19 +25,25 @@ function AppRouter() {
           element={<CreateWorkspacePage />}
         />
 
-        <Route
-          path={ROUTES.JOIN_WORKSPACE}
-          element={<JoinWorkspacePage />}
-        />
+        <Route path={ROUTES.JOIN_WORKSPACE} element={<JoinWorkspacePage />} />
 
-        <Route
-          path={ROUTES.CREATE_PROJECT}
-          element={<CreateProjectPage />}
-        />
+        <Route path={ROUTES.CREATE_PROJECT} element={<CreateProjectPage />} />
 
         <Route
           path={ROUTES.PROJECT_SETTINGS}
           element={<ProjectSettingsPage />}
+        />
+
+        <Route
+          path={ROUTES.TIMEZONE_SETTINGS}
+          element={<TimezoneSettingsPage />}
+        />
+
+        <Route path={ROUTES.MEMBER_SETTINGS} element={<MemberSettingsPage />} />
+
+        <Route
+          path={ROUTES.INTEGRATION_SETTINGS}
+          element={<IntegrationSettingsPage />}
         />
       </Routes>
     </BrowserRouter>
--- src/router/routes.constant.js
+++ src/router/routes.constant.js
@@ -1,7 +1,13 @@
 export const ROUTES = {
   HOME: "/",
+
   CREATE_WORKSPACE: "/workspace/create",
   JOIN_WORKSPACE: "/workspace/join",
+
   CREATE_PROJECT: "/project/create",
   PROJECT_SETTINGS: "/project/settings",
+
+  TIMEZONE_SETTINGS: "/project/settings/timezone",
+  MEMBER_SETTINGS: "/project/settings/members",
+  INTEGRATION_SETTINGS: "/project/settings/integrations",
 };
