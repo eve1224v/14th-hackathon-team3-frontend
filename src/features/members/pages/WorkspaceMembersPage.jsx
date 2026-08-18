29e0f44a307e3868489e489c1e460047e39f9230
Jeonghyeon <mbc5200@gmail.com>
2026-08-19 04:14:38 +0900

    feat: 워크스페이스 멤버 페이지 API 및 관리 기능 연동

---
+++ src/features/members/pages/WorkspaceMembersPage.jsx
@@ -0,0 +1,12 @@
+import MainLayout from "../../../components/MainLayout/MainLayout";
+import WorkspaceMembers from "../components/WorkspaceMembers/WorkspaceMembers";
+
+function WorkspaceMembersPage() {
+  return (
+    <MainLayout>
+      <WorkspaceMembers />
+    </MainLayout>
+  );
+}
+
+export default WorkspaceMembersPage;
